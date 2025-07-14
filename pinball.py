4c1d0d4cf3ea053821d9864e0c26cf8e8a2de085
giuxpp <giuspianfort@gmail.com>
2025-07-13 23:33:20 -0600

    Adding first pygame template

--- pinball.py
+++ pinball.py
@@ -7,17 +7,22 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Mi primer juego")
 
 # Main game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Aquí se actualiza la lógica del juego
-    screen.fill((0, 0, 0))  # Limpiar pantalla (color negro)
+        # Aquí se actualiza la lógica del juego
+        screen.fill((0, 0, 0))  # Limpiar pantalla (color negro)
 
-    # Aquí se dibujan los objetos
-    pygame.display.flip()  # Actualizar pantalla
+        # Aquí se dibujan los objetos
+        pygame.display.flip()  # Actualizar pantalla
 
-# Quit pygame
-pygame.quit()
+    # Quit pygame
+    pygame.quit()
+
+# Execute the main loop
+if __name__ == main:
+    main()
