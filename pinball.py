470b676e81be45cb92f2ad4fc490af812bf9aaff
giuxpp <giuspianfort@gmail.com>
2025-07-14 00:19:40 -0600

    Add bouncing efect on window edges

--- pinball.py
+++ pinball.py
@@ -5,9 +5,12 @@ LOGS_ALLOWED = True
 
 # Global variables
 screen = None
+clock = None
 
 # Ball parameters
 ball_radius = 25
+ball_angle = None
+ball_step = None
 center_x, center_y = 100, 100
 ball_color = (100, 100, 100)
 shadow_color = (180, 180, 180)
@@ -35,28 +38,47 @@ def generate_ball():
 # Function that update the ball posiiton
 # center_x, center_y depending on a given angle and step
 def update_ball_position(angle, step):
-    global center_x, center_y
+    global center_x, center_y, ball_angle, ball_step, ball_radius, screen
+    # Si la bola toca un borde de la pantalla, invertir su dirección
+    if center_x <= ball_radius or center_x >= screen.get_width() - ball_radius:
+        # Invertir la dirección de la bola sentido horizontal
+        ball_angle = (180 - ball_angle) % 360
+        if center_x <= ball_radius:
+            center_x = center_x + ball_radius//2
+        else:
+            center_x = center_x - ball_radius//2
+    if center_y <= ball_radius or center_y >= screen.get_height() - ball_radius:
+        # Invertir la dirección de la bola sentido vertical
+        ball_angle = (-ball_angle) % 360        
+        if center_y >= screen.get_height() - ball_radius: ball_step = 0
+        else: center_y = center_y + ball_radius//2
     # Convert angle to radians
     angle_rad = -math.radians(angle)
     # Update position based on angle and step
     center_x += step * math.cos(angle_rad)
     center_y += step * math.sin(angle_rad)
 
+
+
 # Init game variables
 def game_init():
-    global screen, ball_radius, center_x, center_y, ball_color, shadow_color, highlight_color
+    global screen, ball_radius, center_x, center_y, ball_angle, ball_step
     # Initialize game variables
     center_x, center_y = screen.get_width() // 2, screen.get_height() // 2
+    ball_radius = 25
+    ball_angle = 35  # Initial angle in degrees
+    ball_step = 5  # Step size for ball movement
 
 # Create window
 def create_window():
-    global screen
+    global screen, clock
     screen = pygame.display.set_mode((800, 1000))
     pygame.display.set_caption("Mi primer juego")
+    clock = pygame.time.Clock()
 
 # Main game loop
 def main():
-    global screen
+    global screen, clock, ball_angle, ball_step
     
     # Init pygame and create an initial window
     pygame.init()
@@ -72,12 +94,13 @@ def main():
         # Analyze inputs and handle events
 
         # Aquí se actualiza la lógica del juego
-        update_ball_position(110, 1)
+        update_ball_position(ball_angle, ball_step)
        
         # Aquí se dibujan los objetos
         screen.fill((0, 0, 0))  # Limpiar pantalla (color negro)
         generate_ball()
         pygame.display.flip()  # Actualizar pantalla
+        clock.tick(60)
 
     # Quit pygame
     pygame.quit()
